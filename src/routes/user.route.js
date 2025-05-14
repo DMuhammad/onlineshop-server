82edd20bf4cdfdee6c726884e67b2b44fe9783ce
Dzikri Muhammad <notfnd123@gmail.com>
2025-05-14 21:31:35 +0700

    update: adding middleware for checking auth token

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { updateUser } = require("../controllers/user.controller");
+const verifyAuth = require("../middlewares/verifyAuth");
 const userRoute = express.Router();
 
+userRoute.use(verifyAuth);
 userRoute.put("/:userid/updateProfile", updateUser);
 
 module.exports = userRoute;
