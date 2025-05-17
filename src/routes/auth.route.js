4e4e196a87b6018d54d15db137400f481b95cb38
Dzikri Muhammad <notfnd123@gmail.com>
2025-05-17 21:46:42 +0700

    update: adding check auth

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -6,13 +6,14 @@ const {
   refreshToken,
   changePassword,
 } = require("../controllers/auth.controller");
+const verifyAuth = require("../middlewares/verifyAuth");
 
 const authRoute = express.Router();
 
 authRoute.post("/sign-in", signIn);
 authRoute.post("/sign-up", signUp);
-authRoute.get("/sign-out", signOut);
-authRoute.put("/{:id}/changePassword", changePassword);
-authRoute.get("/refreshToken", refreshToken);
+authRoute.get("/sign-out", verifyAuth, signOut);
+authRoute.put("/{:id}/changePassword", verifyAuth, changePassword);
+authRoute.get("/refreshToken", verifyAuth, refreshToken);
 
 module.exports = authRoute;
