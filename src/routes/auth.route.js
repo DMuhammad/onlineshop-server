8fee8ca017db554a7339f46bcf0b9976a43d23d5
DzikriMuhammad <notfnd123@gmail.com>
2025-06-08 06:22:01 +0700

    use middleware verifyValidUser on update password

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -7,13 +7,19 @@ const {
   changePassword,
 } = require("../controllers/auth.controller");
 const verifyAuth = require("../middlewares/verifyAuth");
+const verifyValidUser = require("../middlewares/verifyValidUser");
 
 const authRoute = express.Router();
 
 authRoute.post("/sign-in", signIn);
 authRoute.post("/sign-up", signUp);
 authRoute.post("/sign-out", verifyAuth, signOut);
-authRoute.put("/{:id}/changePassword", verifyAuth, changePassword);
+authRoute.put(
+  "/{:id}/changePassword",
+  verifyAuth,
+  verifyValidUser,
+  changePassword
+);
 authRoute.get("/refreshToken", refreshToken);
 
 module.exports = authRoute;
