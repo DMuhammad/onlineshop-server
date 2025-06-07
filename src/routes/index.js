c24120b69198ff219b035ac7d89a327a2af59185
DzikriMuhammad <notfnd123@gmail.com>
2025-06-08 06:25:39 +0700

    update: route for discount

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const authRoute = require("./auth.route");
 const userRoute = require("./user.route");
 const categoryRoute = require("./category.route");
+const discountRoute = require("./discount.route");
 
 const router = express.Router();
 
 router.use("/auth", authRoute);
 router.use("/user", userRoute);
 router.use("/category", categoryRoute);
+router.use("/discount", discountRoute);
 
 module.exports = router;
