20cd6582cef0b1fb6ee2b58bdd069e5739192d38
DzikriMuhammad <notfnd123@gmail.com>
2025-06-08 06:23:36 +0700

    update password schema to match with regex, and update the validation error

--- src/dtos/auth.dto.js
+++ src/dtos/auth.dto.js
@@ -1,20 +1,32 @@
 const Joi = require("joi");
 
+const passwordRegex =
+  /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/;
+
 const signUpSchema = Joi.object({
   fullName: Joi.string().min(3).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
+  password: Joi.string().min(8).pattern(passwordRegex).required().messages({
+    "string.pattern.base":
+      "Password must contain at least 1 capital letter, 1 number, and 1 symbol.",
+  }),
   confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
 });
 
 const signInSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
+  password: Joi.string().min(8).pattern(passwordRegex).required().messages({
+    "string.pattern.base":
+      "Password must contain at least 1 capital letter, 1 number, and 1 symbol",
+  }),
 });
 
 const updatePasswordSchema = Joi.object({
   oldPassword: Joi.string().min(8).required(),
-  newPassword: Joi.string().min(8).required(),
+  newPassword: Joi.string().min(8).pattern(passwordRegex).required().messages({
+    "string.pattern.base":
+      "Password must contain at least 1 capital letter, 1 number, and 1 symbol",
+  }),
   confirmPassword: Joi.string().min(8).valid(Joi.ref("newPassword")).required(),
 });
 
@@ -22,9 +34,17 @@ function validateSignUp(data) {
   const { error, value } = signUpSchema.validate(data, { abortEarly: false });
 
   if (error) {
-    const messages = error.details.map((detail) => detail.message);
-    const err = new Error("Validation failed");
-    err.details = messages;
+    const err = {
+      status: "failed",
+      error: {
+        details: error.details.map(({ message, type, context }) => ({
+          message: message.replace(/['"]/g, ""),
+          type,
+          label: context.key,
+        })),
+      },
+    };
+
     return err;
   }
 
@@ -35,9 +55,17 @@ function validateSignIn(data) {
   const { error, value } = signInSchema.validate(data, { abortEarly: false });
 
   if (error) {
-    const messages = error.details.map((detail) => detail.message);
-    const err = new Error("Validation failed");
-    err.details = messages;
+    const err = {
+      status: "failed",
+      error: {
+        details: error.details.map(({ message, type, context }) => ({
+          message: message.replace(/['"]/g, ""),
+          type,
+          label: context.key,
+        })),
+      },
+    };
+
     return err;
   }
 
@@ -50,9 +78,17 @@ function validateUpdatePassword(data) {
   });
 
   if (error) {
-    const messages = error.details.map((detail) => detail.message);
-    const err = new Error("Validate failed");
-    err.details = messages;
+    const err = {
+      status: "failed",
+      error: {
+        details: error.details.map(({ message, type, context }) => ({
+          message: message.replace(/['"]/g, ""),
+          type,
+          label: context.key,
+        })),
+      },
+    };
+
     return err;
   }
 
